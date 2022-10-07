b7dfec101bbe6826ee5c94669f11a49599bbffba
Jayalekshmi K S <jayalekshmiks112@gmail.com>
2022-10-07 23:49:08 +0530

    Checking format done

--- Alarm.py
+++ Alarm.py
@@ -1,7 +1,32 @@
 from datetime import datetime
 from playsound import playsound
+import time
 
+def check(t) :
+    if len(t) != 11:
+        return "Invalid format"
+    else:
+        if int(t[0:2])>12:
+            time.sleep(1)
+            return "Hour format is invalid"
+        elif int(t[3:5])>59:
+            time.sleep(1)
+            return "Minute format is invalid"
+        elif int(t[6:8])>59:
+            time.sleep(1)
+            return "Seconds format is invalid"
+        else:
+            time.sleep(1)
+            return "Setting.."
 while True:
-    time=input("Enter time in format HH:MM:SS AM/PM: ")
-    print("Alarm set for time", time)
-    break
+    t = input("Enter time in format HH:MM:SS AM/PM: ")
+    result = check(t)
+    if result != "Setting..":
+        print(result)
+    else:
+        print("Alarm set for time", t)
+        break
+
+
+
+
