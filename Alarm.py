34c0a39b9f79d40672d71332c4080ccd9e1a7f83
Jayalekshmi K S <jayalekshmiks112@gmail.com>
2022-10-07 23:08:28 +0530

    Initial commit: Basic format done

---
+++ Alarm.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from playsound import playsound
+
+while True:
+    time=input("Enter time in format HH:MM:SS AM/PM: ")
+    print("Alarm set for time", time)
+    break
