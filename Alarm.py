066a6202e2659bd62554750e52f430ab9492d5eb
Jayalekshmi K S <jayalekshmiks112@gmail.com>
2022-10-08 16:59:16 +0530

    Comparison is done

--- Alarm.py
+++ Alarm.py
@@ -6,18 +6,19 @@ def check(t) :
     if len(t) != 11:
         return "Invalid format"
     else:
-        if int(t[0:2])>12:
+        if int(t[0:2]) > 12:
             time.sleep(1)
-            return "Hour format is invalid"
-        elif int(t[3:5])>59:
+            return("Hour format is invalid")
+        if int(t[3:5]) > 59:
             time.sleep(1)
-            return "Minute format is invalid"
-        elif int(t[6:8])>59:
+            return("Minute format is invalid")
+        if int(t[6:8]) > 59:
             time.sleep(1)
-            return "Seconds format is invalid"
+            return("Seconds format is invalid")
         else:
             time.sleep(1)
-            return "Setting.."
+            return("Setting..")
+
 while True:
     t = input("Enter time in format HH:MM:SS AM/PM: ")
     result = check(t)
@@ -27,6 +28,25 @@ while True:
         print("Alarm set for time", t)
         break
 
+hour = t[0:2]
+min = t[3:5]
+sec = t[6:8]
+period = t[9:].upper()
+
+while True:
+    now = datetime.now()
+
+    c_hour = now.strftime("%I")
+    c_min = now.strftime("%M")
+    c_sec = now.strftime("%S")
+    c_period = now.strftime("%p")
+
+    if period == c_period:
+        if hour == c_hour:
+            if min == c_min:
+                if sec == c_sec:
+                    print("Wake up!!!!")
+                    break
 
 
 
